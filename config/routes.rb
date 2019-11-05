792e0f4d327299835bae7d397b26603a1a9f88c1
Hazrian <chazotflorian@gmail.com>
2019-11-05 16:01:14 +0100

    [add] user index and show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
   get 'static_pages/home'
   devise_for :users, path: '', path_names: { sign_in: 'connexion', sign_out: 'deconnexion'}, :controllers => { registrations: 'registrations' }
-  resources :users, :path => "utilisateur"
+  resources :users, only: [:show, :index], :path => "utilisateur"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
