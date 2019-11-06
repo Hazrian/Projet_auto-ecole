172d9eaaacf6e29d694c1e09be879cecebe552e5
Hazrian <chazotflorian@gmail.com>
2019-11-06 11:44:59 +0100

    [add] restricted access to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
+  before_action :user_is_user_to_show, only: [:show]
+  before_action :user_is_admin, only: [:index]
   def index
   end
 
   def show
     @user = User.find(params['id'])
+  end
+
+  private
+
+  def user_is_user_to_show
+    unless current_user == User.find(params[:id])
+      flash[:error] = "Tu ne peux pas accéder aux profils des autres utilisateurs"
+      redirect_to :root
+    end
   end
 end
