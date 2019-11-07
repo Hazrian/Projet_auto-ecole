13cd8bf4d41338d0245de550ac2a072cfcf02e8b
Hazrian <chazotflorian@gmail.com>
2019-11-07 14:18:33 +0100

    [update] formation type routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'formation_types/new'
-  get 'formation_types/create'
-  get 'formation_types/update'
-  get 'formation_types/edit'
-  get 'formation_types/destroy'
-  get 'formation_types/show'
-  get 'formation_types/index'
+  resources :formation_types
   root to: 'static_pages#home'
   get 'static_pages/home'
   devise_for :users, path: '', path_names: { sign_in: 'connexion', sign_out: 'deconnexion'}, :controllers => { registrations: 'registrations' }
