d5b0423055e8b9dd5f283431668025ab57f64e79
Hazrian <chazotflorian@gmail.com>
2019-11-07 13:23:11 +0100

    [add] mailer production configuration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
     @user = user
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = url_for controller: 'registrations', action: 'new', host: 'localhost', port: '3000'%>
+    @url  = url_for controller: 'registrations', action: 'new', host: 'localhost', port: '3000'
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue !')
