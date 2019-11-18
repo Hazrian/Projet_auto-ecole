ae8c340a5129375f54f2048559a7e481b37f5410
Hazrian <chazotflorian@gmail.com>
2019-11-18 15:47:26 +0100

    aze

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,9 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {
-    :host => ENV['APP_URL']
-  }
   config.action_mailer.delivery_method = :mailjet
 
 end
